12d457ff573f57aea99195ac5e29f036e4201699
Muhammad424 <135837731+Muhammad424@users.noreply.github.com>
2023-06-21 18:06:47 +0700

    Update Sewy.lua

--- Sewy.lua
+++ Sewy.lua
@@ -53,16 +53,7 @@ local Window = Rayfield:CreateWindow({
     end,
  })
 
- local Toggle = Tab:CreateToggle({
-    Name = "Turn Off",
-    CurrentValue = false,
-    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-    -- The function that takes place when the toggle is pressed
-    -- The variable (Value) is a boolean on whether the toggle is true or false
-    end,
- })
-
+ 
 
 
 
